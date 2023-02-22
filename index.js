459071eb6776f927733d6b73da87349f74ea6a97
DwightDiesmo <deightdiesmo@gmail.com>
2023-02-21 19:04:51 -0800

    init

---
+++ index.js
@@ -0,0 +1,141 @@
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema, assertInputType } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+var restaurants = [
+  {
+    id: 1,
+    name: "WoodsHill ",
+    description:
+      "American cuisine, farm to table, with fresh produce every day",
+    dishes: [
+      {
+        name: "Swordfish grill",
+        price: 27,
+      },
+      {
+        name: "Roasted Broccily ",
+        price: 11,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Fiorellas",
+    description:
+      "Italian-American home cooked food with fresh pasta and sauces",
+    dishes: [
+      {
+        name: "Flatbread",
+        price: 14,
+      },
+      {
+        name: "Carbonara",
+        price: 18,
+      },
+      {
+        name: "Spaghetti",
+        price: 19,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Karma",
+    description:
+      "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
+    dishes: [
+      {
+        name: "Dragon Roll",
+        price: 12,
+      },
+      {
+        name: "Pancake roll ",
+        price: 11,
+      },
+      {
+        name: "Cod cakes",
+        price: 13,
+      },
+    ],
+  },
+];
+var schema = buildSchema(`
+type Query{
+  restaurant(id: Int): restaurant
+  restaurants: [restaurant]
+},
+type restaurant {
+  id: Int
+  name: String
+  description: String
+  dishes:[Dish]
+}
+type Dish{
+  name: String
+  price: Int
+}
+input restaurantInput{
+  name: String
+  description: String
+}
+type DeleteResponse{
+  ok: Boolean!
+}
+type Mutation{
+  setrestaurant(input: restaurantInput): restaurant
+  deleterestaurant(id: Int!): DeleteResponse
+  editrestaurant(id: Int!, name: String!): restaurant
+}
+`);
+// The root provides a resolver function for each API endpoint
+
+var root = {
+  restaurant: (arg) => {
+    restaurant = restaurants.filter((restaurant) => restaurant.id == arg.id);
+    console.log(restaurant)
+    // console.log(arg.id)
+    // console.log(restaurants[arg.id])
+    return restaurant[0]
+  },
+  restaurants: () => {
+    return restaurants
+  },
+  setrestaurant: ({ input }) => {
+    restaurants.push({ name: input.name, email: input.email, age: input.age });
+    return input;
+  },
+  deleterestaurant: ({ id }) => {
+    const ok = Boolean(restaurants[id]);
+    let target_restaurant = restaurants[id];
+    restaurants = restaurants.filter((restaurant) => restaurant.id !== id);
+    console.log(JSON.stringify(target_restaurant));
+    console.log(ok)
+    return { ok }
+  },
+  editrestaurant: ({ id, ...restaurant }) => {
+    target_restaurant = restaurants.filter((restaurant) => restaurant.id == id);
+
+    if (!target_restaurant[0]) {
+      throw new Error("restaurant doesn't exist");
+    }
+    restaurants[target_restaurant[0].id-1] = {
+      ...restaurants[target_restaurant[0].id-1],
+      ...restaurant,
+    };
+    return restaurants[target_restaurant[0].id-1];
+  },
+};
+var app = express();
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+var port = 5500;
+app.listen(5500, () => console.log("Running Graphql on Port:" + port));
+console.log("http://localhost:5500/graphql")
